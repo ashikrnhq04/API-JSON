33ca8417806fcbe7a88e9ee4d31c536b9e64bba1
ashikrnhq04 <ashikrn.hq04@gmail.com>
2025-08-02 03:30:53 +0600

    refactor: database.php

--- config/database.php
+++ config/database.php
@@ -2,7 +2,7 @@
 
 return [
     "database" => [
-        "dbname" => $_ENV["DB_NAME"] ?? "commercio_db",
+        "dbname" => $_ENV["DB_NAME"] ?? "test_db",
         "host" => $_ENV["DB_HOST"] ?? "localhost",
         "port" => (int)($_ENV["DB_PORT"] ?? 3306),
         "charset" => $_ENV["DB_CHARSET"] ?? "utf8mb4",
